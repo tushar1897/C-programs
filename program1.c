09bdccf3bb3af4b9c6acb1bd536d7323e9ce83e8
tushar1897 <85217770+tushar1897@users.noreply.github.com>
2021-06-23 20:12:02 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("jay ganesh\n");
+
+    //return 0;
+    return 1;
+}
+
+//return 0;           program successful 
+//return 1;           abortive termintion       
+//return -1;          erronious termination     
